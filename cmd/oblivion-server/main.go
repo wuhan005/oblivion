018006937bf30100e15beee7e8224c8a73c567c8
E99p1ant <i@github.red>
2022-03-04 19:16:13 +0800

    update k8s client connection

--- cmd/oblivion-server/main.go
+++ cmd/oblivion-server/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
+	"net"
+	"os"
+
 	"github.com/flamego/flamego"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -18,11 +22,21 @@ func main() {
 		panic(err)
 	}
 
-	config, err := rest.InClusterConfig()
+	const (
+		tokenFile = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	)
+	host, port := os.Getenv("KUBERNETES_SERVICE_HOST"), os.Getenv("KUBERNETES_SERVICE_PORT")
+	token, err := ioutil.ReadFile(tokenFile)
 	if err != nil {
-		log.Fatal("Failed to get in cluster config: %v", err)
+		log.Fatal("Failed to get token: %v", err)
 	}
-	k8sClient, err := kubernetes.NewForConfig(config)
+
+	k8sClient, err := kubernetes.NewForConfig(&rest.Config{
+		Host:            "http://" + net.JoinHostPort(host, port),
+		TLSClientConfig: rest.TLSClientConfig{},
+		BearerToken:     string(token),
+		BearerTokenFile: tokenFile,
+	})
 	if err != nil {
 		log.Fatal("Failed to get k8s client: %v", err)
 	}
